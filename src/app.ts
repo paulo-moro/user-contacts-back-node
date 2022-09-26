81595efa0facb75abcffb1df8ced6ff147444681
Paulo Henrique Moro dos Santos <paulo.morolol@gmail.com>
2022-09-26 12:26:48 -0300

    fixed cors.

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,13 @@ import express from "express"
 import { handleError } from "./middlewares/error.middleware"
 import { appRoutes } from "./router"
 import swaggerDocs from "./swagger.json"
-
+import cors from "cors"
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
+
 
 app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
